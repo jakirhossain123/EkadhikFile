63192706f69139e74ed229f0009784447dd8fea6
jakirhossain123 <69891873+jakirhossain123@users.noreply.github.com>
2022-06-14 15:40:01 +0600

    Update main.js
    Update check duplicate image.
    Update image delete by server link

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-let duplicateCheckArray = [];
+var ekadhikFileDuplicateCheckArray = [];
 $(document).ready(function() {
 
     $(".ekadhik-input").each(function (){
-       let getName = $(this).attr('name');
-       $(this).attr('data-name', getName);
-       $(this).removeAttr('name');
+        let thisIndex = $(this).index();
+        let getName = $(this).attr('name');
+        $(this).attr('data-name', getName);
+        $(this).removeAttr('name');
     });
 
 
@@ -25,20 +26,21 @@ $(document).ready(function() {
             event.preventDefault();
             event.stopPropagation();
             let self = $(this);
-            let attrName = $(this).parents('.ekadhik-field').find('.ekadhik-input').attr('data-name');
+            let targetInput = $(this).parents('.ekadhik-field').find('.ekadhik-input');
+            let attrName = targetInput.attr('data-name');
             if(event.originalEvent.dataTransfer){
                 if(event.originalEvent.dataTransfer.files.length) {
                     event.preventDefault();
                     event.stopPropagation();
                     /*UPLOAD FILES HERE*/
-                    upload(event.originalEvent.dataTransfer.files, attrName, self);
+                    upload(event.originalEvent.dataTransfer.files, attrName, targetInput);
                 }
             }
-            $(this).parents('.ekadhik-field').removeClass('dragging');
+            self.parents('.ekadhik-field').removeClass('dragging');
         });
 
         // =============== On Trigger =====================
-        $(".ekadhik-input").on("change", function (e) {
+        $(document).on("change",".ekadhik-input", function (e) {
             let self = $(this);
             let attrName = $(this).attr('data-name');
             upload(this.files, attrName, self);
@@ -50,65 +52,94 @@ $(document).ready(function() {
     // ================= Function for upload image =====================
     function upload(files, attrName, self){
         let inputName = attrName;
-        var uploadItemLength = self.parents('.ekadhik-field').find('.upload-item').length;
         let thisAfterList = self.parents('.ekadhik-field').find('.selected-file-list');
+        let fileLimitAttr = self.attr('max');
+        let fileLimit = null;
+        if (typeof fileLimitAttr != null && typeof fileLimitAttr != 'undefined')
+        {
+            fileLimit = parseInt(fileLimitAttr);
+        }
 
         for(let i=0; i<files.length; i++) {
-
-            if (duplicateCheckArray.indexOf(files[i].name) > -1) {
-                alert(files[i].name+ ' already selected.');
+            let uploadItemLength = self.parents('.ekadhik-field').find('.selected-ekadik-item').length;
+            if (fileLimit != null && uploadItemLength >= fileLimit) {
+                alert('You can upload maximum' + ' ( ' + fileLimit + ' ) ' + 'image');
+                return false;
             } else {
+                if (ekadhikFileDuplicateCheckArray.indexOf(attrName+files[i].name) > -1) {
+                    alert(files[i].name + ' already selected.');
+                } else {
+                    let dt = new DataTransfer();
+                    let f = files[i];
+                    // console.log(f);
+                    dt.items.add(
+                        new File(
+                            [f.slice(0, f.size, f.type)],
+                            f.name
+                        ));
+                    self.parents('.ekadhik-field').find('.selected-file-list').append(
+                        `
+                            <li class="selected-ekadik-item">
+                                <div class="selected-ekadik-item-content">
+                                    <input type="file" name="` + inputName + `" class="d-none attach-file-value" id="attachFile` + i + `">
+                                    <button type='button' class="remove-ekadik-item"><span class='text'>&times</span></button>
+                                </div>
+                            </li>
+                        `
+                    );
+                    let fileReader = new FileReader();
+                    fileReader.onload = (function (j) {
+                        let file = j.target;
+                        $("#attachFile" + i).after("<img class=\"uploaded-file\" src=\"" + j.target.result + "\" alt='No preview'/>").removeAttr('id');
+                        $("#attachFile" + i).removeAttr('id')
+                    });
+                    fileReader.readAsDataURL(f);
 
-
-                let dt = new DataTransfer();
-                let f = files[i];
-                // console.log(f);
-                dt.items.add(
-                    new File(
-                        [f.slice(0, f.size, f.type)],
-                        f.name
-                    ));
-
-
-
-                self.parents('.ekadhik-field').find('.selected-file-list').append(
-                    `
-                        <li class="selected-ekadik-item">
-                            <div class="selected-ekadik-item-content">
-                                <input type="file" name="`+ inputName +`" class="d-none attach-file-value" id="attachFile`+i+`">
-                                <button type='button' class="remove-ekadik-item"><span class='text'>&times</span></button>
-                            </div>
-                        </li>
-                    `
-                );
-
-
-                let fileReader = new FileReader();
-                fileReader.onload = (function (j) {
-                    let file = j.target;
-                    $("#attachFile"+i).after("<img class=\"uploaded-file\" src=\"" + j.target.result + "\" alt='No preview'/>").removeAttr('id');
-                    $("#attachFile"+i).removeAttr('id')
-                });
-                fileReader.readAsDataURL(f);
-
-                var back = document.getElementById("attachFile"+i);
-                back.files = dt.files;
+                    var back = document.getElementById("attachFile" + i);
+                    back.files = dt.files;
+                }
+                ekadhikFileDuplicateCheckArray.push(attrName+files[i].name);
             }
-            duplicateCheckArray.push(files[i].name);
         }
         self.parents('.ekadhik-field').find('.ekadhik-input').val('');
     }
 });
 
-$(document).on('click', '.remove-ekadik-item', function () {
+// ========== Click For Remove Item =============
+$(document).on('click', '.remove-ekadik-item', function (e) {
+    e.preventDefault();
+    let self = $(this);
+    let targetUrl = self.attr('data-remove-url');
+    let parseUrl = null;
+    if (typeof targetUrl != null && typeof targetUrl != 'undefined')
+    {
+        parseUrl = targetUrl;
+    }
+    // ====== Remove Item / Also from server if exist url ============
+    if (parseUrl == null)
+    {
+        $(this).parents(".selected-ekadik-item").remove();
+    } else {
+        // ======= If Dev Url Exist then work for developer ========
+        $.ajax({
+            type: 'GET',
+            url: parseUrl,
+            success: function (info) {
+                self.parents('.selected-ekadik-item').remove();
+                console.log('success');
+            }
+        });
+    }
+
+    // ==== Remove From Duplicate Array ====
     if ($(this).parents(".selected-ekadik-item").find('input[type="file"]').length > 0)
     {
+        let matchInputAttr = $(this).parents(".selected-ekadik-item").find('input[type="file"]').attr('name');
         var getVal = $(this).parents(".selected-ekadik-item").find('input[type="file"]').val();
-
-        var getName = getVal.replace(/C:\\fakepath\\/i, '');
-        duplicateCheckArray = jQuery.grep(duplicateCheckArray, function(value) {
+        var getName = matchInputAttr+getVal.replace(/C:\\fakepath\\/i, '');
+        ekadhikFileDuplicateCheckArray = jQuery.grep(ekadhikFileDuplicateCheckArray, function(value) {
             return value != getName;
         });
     }
-    $(this).parents(".selected-ekadik-item").remove();
+
 });
